a5b8c0705d3bfe1e0677b0db778f7dcec4f2fb9f
Barry Goldberg <bgold6562@gmail.com>
2025-11-03 11:59:53 +0200

    errors_handled

--- utils/errors.js
+++ utils/errors.js
@@ -1,11 +1,15 @@
-const { BAD_REQUEST, NOT_FOUND, INTERNAL_SERVER_ERROR } = require("./constants");
+const {
+  BAD_REQUEST,
+  NOT_FOUND,
+  INTERNAL_SERVER_ERROR,
+} = require("./constants");
 
 // Error handling middleware
 const handleError = (err, req, res, _next) => {
   // Default error
   let status = INTERNAL_SERVER_ERROR;
   let message = "Internal Server Error";
-  
+
   // Handle specific error types
   if (err.name === "ValidationError") {
     status = BAD_REQUEST;
@@ -20,14 +24,15 @@ const handleError = (err, req, res, _next) => {
     status = err.status;
     message = err.message;
   }
-  
+
   return res.status(status).json({
     message,
   });
 };
 
 // 404 handler
-const handleNotFound = (req, res) => res.status(NOT_FOUND).json({ message: "Route not found" });
+const handleNotFound = (req, res) =>
+  res.status(NOT_FOUND).json({ message: "Route not found" });
 
 module.exports = {
   handleError,
