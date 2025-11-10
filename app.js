5e6933b771dba6a582e1ea98d14749f7e0e63e8a
Barry Goldberg <bgold6562@gmail.com>
2025-11-10 18:25:28 +0200

    third_of_the_night

--- app.js
+++ app.js
@@ -12,6 +12,13 @@ const { PORT = 3001, MONGODB_URI = "mongodb://127.0.0.1:27017/wtwr_db" } =
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  if (!req.user) {
+    req.user = { _id: "5d8b8592978f8bd833ca8133" };
+  }
+  next();
+});
+
 app.use("/", routes);
 
 app.use(handleNotFound);
